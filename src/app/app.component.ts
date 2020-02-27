d156f805559207c16543a36813f1323c9dd68fdc
M@! <yoat42@gmail.com>
2020-02-27 00:13:45 -0500

    Hopefully importing mediaelement.
    https://stackoverflow.com/questions/56301380/how-to-use-mediaelement-in-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { ElectronService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { AppConfig } from '../environments/environment';
+declare var MediaElementPlayer;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('mediaPlayer') mediaPlayerElement: ElementRef;
+  public mediaPlayer;
+
   constructor(
     public electronService: ElectronService,
     private translate: TranslateService
@@ -25,4 +29,17 @@ export class AppComponent {
       console.log('Mode web');
     }
   }
+
+  ngOnInit() {
+    
+  }
+
+  ngAfterViewInit() {
+    this.loadMediaPlayer();
+  }
+
+  loadMediaPlayer() {
+    this.mediaPlayer = new MediaElementPlayer(this.mediaPlayerElement.nativeElement);
+  }
+
 }
