f836e00770300e97fea14b95b0fc475c0d43193e
M@! <yoat42@gmail.com>
2020-02-28 21:27:20 -0500

    Smaller size and disable security - allow local file loading!

--- main.ts
+++ main.ts
@@ -15,11 +15,12 @@ function createWindow(): BrowserWindow {
   win = new BrowserWindow({
     x: 0,
     y: 0,
-    width: size.width,
-    height: size.height,
+    width: 500, // size.width,
+    height: 400, // size.height,
     webPreferences: {
       nodeIntegration: true,
-      allowRunningInsecureContent: (serve) ? true : false,
+      webSecurity: false,
+      // allowRunningInsecureContent: (serve) ? true : false,
     },
   });
 
