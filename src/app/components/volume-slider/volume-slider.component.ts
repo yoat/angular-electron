338469db3672c62b392c7cb66dbb72af6fd8f962
M@! <yoat42@gmail.com>
2020-03-01 17:25:48 -0500

    Stubbed in value control and response.

--- src/app/components/volume-slider/volume-slider.component.ts
+++ src/app/components/volume-slider/volume-slider.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'VolumeSlider',
@@ -6,11 +7,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './volume-slider.component.html',
   styleUrls: ['./volume-slider.component.css']
 })
-export class VolumeSliderComponent implements OnInit {
+export class VolumeSliderComponent implements OnInit, OnDestroy {
+  @ViewChild('volume', { static: true }) vcVolume: ElementRef;
+  volValue: number;
+  private sub: Subscription;
 
-  constructor() { }
+  constructor() {
+    this.volValue = 70;
+   }
 
   ngOnInit(): void {
+    this.sub = fromEvent(this.vcVolume.nativeElement, 'input')
+      .subscribe((x) => {
+        console.log(`volume: ${this.vcVolume.nativeElement.value}`);
+        this.volValue = this.vcVolume.nativeElement.value;
+      });
   }
 
+  ngOnDestroy() {
+    this.sub?.unsubscribe();
+  }
 }
