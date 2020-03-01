a307c0904772a954c74fba91731acb34e5e3aeab
M@! <yoat42@gmail.com>
2020-02-29 22:54:20 -0500

    Rough save and load, with initial empty list cases handled.

--- src/app/services/playlist.service.ts
+++ src/app/services/playlist.service.ts
@@ -16,18 +16,18 @@ export class PlaylistService {
   private shuffleMode: string;
   private repeatMode: string;
   private index: number = 0;
-
-  private data: Array<Track> = [
-    new Track({
-      filepath: 'X:\\Music\\AllttA - The Upper Hand\\01 - AllttA (feat. 20syl & Mr. J. Medeiros).mp3',
-      trackId: 1,
-      trackName: "AllttA (feat. 20syl & Mr. J. Medeiros)",
-      artistId: 1,
-      artistName: "AllttA",
-      albumId: 1,
-      albumName: "The Upper Hand",
-    })
-  ];
+  private _dataKey = "playlistService-data-v1";
+  private data: Array<Track> = [];
+  //   new Track({
+  //     filepath: 'X:\\Music\\AllttA - The Upper Hand\\01 - AllttA (feat. 20syl & Mr. J. Medeiros).mp3',
+  //     trackId: 1,
+  //     trackName: "AllttA (feat. 20syl & Mr. J. Medeiros)",
+  //     artistId: 1,
+  //     artistName: "AllttA",
+  //     albumId: 1,
+  //     albumName: "The Upper Hand",
+  //   })
+  // ];
 
   constructor(private playback: PlaybackService) {
     // only subscribe to Stopped event.
@@ -37,7 +37,26 @@ export class PlaylistService {
     //   console.log(`Track ended, triggering next track...`);
     //   this.nextTrack();
     // });
-   }
+    this.index = 0;
+    setTimeout(() => {
+      this.load();
+      if (this.data.length > 0) {
+        this.playback.load(this.data[this.index]);
+      }
+    },100);
+    
+  }
+
+  load() {
+    console.log(`load...`);
+    const temp: Array<Track> = JSON.parse(localStorage.getItem(this._dataKey));
+    this.data = temp || [];
+  }
+
+  save() {
+    console.log(`save...`);
+    localStorage.setItem(this._dataKey, JSON.stringify(this.data));
+  }
 
   shuffle(mode: string) {
     this.shuffleMode = mode;
@@ -48,27 +67,31 @@ export class PlaylistService {
   }
 
   nextTrack() {
-    this.playback.stop();
-    // this.playback.load(this.data[0]);
-    if (this.data.length > 1) {
-      this.index = (this.index >= this.data.length - 1) ? 0 : this.index + 1;
+    if (this.data.length > 0) {
+      this.playback.stop();
+      // this.playback.load(this.data[0]);
+      if (this.data.length > 1) {
+        this.index = (this.index >= this.data.length - 1) ? 0 : this.index + 1;
+      }
+      // consult shuffle and repeat settings, then call playback.load()
+      // if the pause between tracks is unacceptable, preloading is ez
+      this.playback.load(this.data[this.index]);
+      this.playback.play();
     }
-    // consult shuffle and repeat settings, then call playback.load()
-    // if the pause between tracks is unacceptable, preloading is ez
-    this.playback.load(this.data[this.index]);
-    this.playback.play();
   }
 
   prevTrack() {
-    this.playback.stop();
-    // this.playback.load(this.data[0]);
-    if (this.data.length > 1) {
-      this.index = (this.index <= 0) ? this.data.length - 1 : this.index - 1;
+    if (this.data.length > 0) {
+      this.playback.stop();
+      // this.playback.load(this.data[0]);
+      if (this.data.length > 1) {
+        this.index = (this.index <= 0) ? this.data.length - 1 : this.index - 1;
+      }
+      // maintain a history of played tracks,
+      // this loads previous state instead of randomiizing in reverse
+      this.playback.load(new Track(this.data[this.index]));
+      this.playback.play();
     }
-    // maintain a history of played tracks,
-    // this loads previous state instead of randomiizing in reverse
-    this.playback.load(new Track(this.data[this.index]));
-    this.playback.play();
   }
 
   async importFile(filepath: string) {
@@ -85,6 +108,10 @@ export class PlaylistService {
         albumName: metadata.common.album,
       });
       this.data.push(temp);
+      this.save();
+      if (this.data.length == 1) {
+        this.playback.load(this.data[this.index]);
+      }
     } catch (ex) {
       console.log(`EX: ${JSON.stringify(ex)}`);
       
