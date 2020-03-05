5deff0081294d57cf2ca4a8b6a4c3381b12cbec5
M@! <yoat42@gmail.com>
2020-03-05 00:00:26 -0500

    Put in subject and observable with sending method to connect nodes.

--- src/app/services/ipc.service.ts
+++ src/app/services/ipc.service.ts
@@ -1,3 +1,4 @@
+import { Subject } from 'rxjs';
 import { IpcMessage } from './../models/ipc.model';
 import { ipcRenderer, IpcRendererEvent } from 'electron';
 import { PlaybackService } from './../services/playback.service';
@@ -8,12 +9,24 @@ import { Injectable } from '@angular/core';
 })
 export class IpcService {
   private channel = 'cc-ipc-msg';
+  private messageSource = new Subject<IpcMessage>();
+  message$ = this.messageSource.asObservable();
 
   constructor() { 
     ipcRenderer.on(this.channel, (event: IpcRendererEvent, args: IpcMessage) => {
       console.log("renderer.js::" + JSON.stringify(args)); // prints "ping"
-      // my.load(arg.path);
+      this.messageSource.next(args);
     });
     console.log(`IpcService initialized!`);
+  }
+
+  send(source: string, target: string, event: string, data: any) {
+    const payload: IpcMessage = {
+      source,
+      target,
+      event,
+      data
+    };
+    ipcRenderer.send(this.channel, payload);
   }
 }
