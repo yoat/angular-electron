07ef1c61173a48a277b3c5d695ca913497657552
M@! <yoat42@gmail.com>
2020-03-07 00:22:36 -0500

    Sample projecting mute button through ProButton.

--- src/app/components/pro-button/pro-button.component.ts
+++ src/app/components/pro-button/pro-button.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pro-button',
+  selector: 'ProButton',
   templateUrl: './pro-button.component.html',
-  styleUrls: ['./pro-button.component.css']
+  styleUrls: ['./pro-button.component.scss']
 })
 export class ProButtonComponent implements OnInit {
 
