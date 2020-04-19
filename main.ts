20e450ad1dbcdb1f3dcd51bc6d60b3c0687d9003
M@! <yoat42@gmail.com>
2020-04-19 13:32:31 -0400

    Preload windows from main (temp) via createWindow.

--- main.ts
+++ main.ts
@@ -15,53 +15,6 @@ const wins: WinDict = {};
 const args = process.argv.slice(1),
     serve = args.some(val => val === '--serve');
 
-function showWindow(name) {
-  if (!wins[name]) {
-    let myWin = new BrowserWindow({
-      webPreferences: {
-        experimentalFeatures: true,
-        nodeIntegration: true,
-        webSecurity: false,
-      },
-      // width: 800,
-      // height: 600,
-      // frame: false,
-      titleBarStyle: 'default',
-      icon: iconPath,
-      show: false,
-    });
-
-    if (serve) {
-      myWin.loadURL(url.format({
-        pathname: 'localhost:4242',
-        protocol: 'http:',
-        slashes: true,
-        hash: `/${name}`
-      }));
-    } else {
-      myWin.loadURL(url.format({
-        pathname: path.join(__dirname, 'dist', 'index.html'),
-        protocol: 'file:',
-        slashes: true,
-        hash: `/${name}`
-      }));
-    }
-   
-    // console.log(`Path: '${path.join(__dirname, 'playlist.html')}'`);
-    
-    myWin.on('closed', function () {
-      wins[name] = null;
-    });
-    wins[name] = myWin;
-    wins[name].on('ready-to-show', () => {
-      wins[name].show();
-      wins[name].webContents.openDevTools();
-    });
-  } else {
-    wins[name].show();
-  }
-};
-
 function hideWindow(name) {
   if (wins[name]) {
     wins[name].hide();
@@ -75,6 +28,11 @@ function minWindow(name) {
 };
 
 function createWindow(args: ICreateWindow) {
+  const name = args.name;
+  if (!name) {
+    console.warn('invalid name supplied to createWindow');
+    return;
+  }
   // initial configuration
   const options: Electron.BrowserWindowConstructorOptions = {
     webPreferences: {
@@ -84,8 +42,8 @@ function createWindow(args: ICreateWindow) {
     },
     // frame: false,
     titleBarStyle: 'default',
-    icon: iconPath,
     show: false,
+    icon: iconPath,
   };
   if (args.position) {
     options.x = args.position.x;
@@ -198,6 +156,10 @@ function createMainWindow(): BrowserWindow {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show();
+
+    // init others
+    createWindow({ name: 'viz', debug: true, show: false });
+    createWindow({ name: 'playlist', debug: true, show: false });
   });
 
   ipcMain.on('webamp-loadpath', (event, arg) => {
@@ -262,7 +224,7 @@ ipcMain.on('showWindow', (ev, arg) => {
   switch (name) {
     case 'playlist':
     case 'viz':
-      showWindow(name);
+      wins[name].show();
       break;
     default:
       break;
